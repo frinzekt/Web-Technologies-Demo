aa7bce70f21c9271a9552f04e54d1744dc2d3ecf
frinzekt <frinzekt@gmail.com>
2020-04-14 20:19:47 +0800

    bundling

--- app/index.js
+++ app/index.js
@@ -1,4 +1,14 @@
+//GLOBAL VARIABLES
+window.isDOMViewEnabled = false;
+
+
 import Appender from './HTMLappender';
 
+// Component JS
 import Nav from './components/Nav';
 import Footer from './components/Footer';
+import Sidebar from './components/Sidebar';
+
+// Event-Handling JS
+import sidebar from '../js/sidebar';
+import nav from '../js/nav';
