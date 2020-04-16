cda85677391cb1635d968c6af9d43b8ff996a016
frinzekt <frinzekt@gmail.com>
2020-04-16 10:35:03 +0800

    Dom responsiveness and keypress

--- js/sidebar.js
+++ js/sidebar.js
@@ -1,6 +1,5 @@
 hljs.configure({ tabReplace: ' ' }); // config of code spacing
 
-
 const highlightCode = () => {
 	document.querySelectorAll('pre code').forEach((block) => {
 		hljs.highlightBlock(block);
@@ -19,9 +18,17 @@ document.onmouseover = (e) => {
 		) {
 			// Only highlight outside code box to avoid recursion
 			//CHANGE TEXT AND HIGHLIGHT ELEMENT
+			// ALSO REMOVE PREVIOUS OVERLAY
+			try {
+				window.lastOverlay.classList.remove('overlay');
+			} catch (e) {
+				//SILENT ERROR
+			}
+
 			codeDiv.textContent = e.target.outerHTML;
 			highlightCode();
 			e.target.classList.add('overlay');
+			window.lastOverlay = e.target;
 		}
 	}
 };
@@ -30,13 +37,29 @@ document.onmouseout = (e) => {
 		e.target.classList.remove('overlay');
 	}
 };
-
-$('#menu-toggle').click((e) => {
+const toggleDOMViewEnabled = () => {
 	isDOMViewEnabled = !isDOMViewEnabled;
+	navIndicator = document.querySelector('.html-inspector');
+
+	//RESPONSIVE AND COLOR
+	if (isDOMViewEnabled) {
+		navIndicator.style.color = '#88C559';
+		[...document.querySelectorAll('.song')].forEach((element) => element.classList.remove('col-md-6'));
+		[...document.querySelectorAll('.song-preview')].forEach((element) => element.classList.remove('col-md-5'));
+	} else {
+		navIndicator.style.color = 'inherit';
+		[...document.querySelectorAll('.song')].forEach((element) => element.classList.add('col-md-6'));
+		[...document.querySelectorAll('.song-preview')].forEach((element) => element.classList.add('col-md-5'));
+	}
+};
+$('#menu-toggle').click((e) => {
 	e.preventDefault();
 	$('#wrapper').toggleClass('toggled');
+	toggleDOMViewEnabled();
 });
 
 $('body').keypress((e) => {
-	isDOMViewEnabled = !isDOMViewEnabled;
+	if (e.key.toLowerCase() == 'f') {
+		toggleDOMViewEnabled();
+	}
 });
